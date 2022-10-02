68fd68a1b7410c3632d8329082e6605e481f2cdb
Bossvanlindt <yann.bonzom11@gmail.com>
2022-10-01 20:08:40 -0400

    chart done

--- client/src/App.js
+++ client/src/App.js
@@ -8,30 +8,62 @@ const App = () => {
   const [submitted, setSubmitted] = useState(false);
   const [nutriInfo, setNutriInfo] = useState([]);
 
-  useState(async () => {
-    const cur_nutriInfo = await foodHandler.getNutriInfo();
-    const cur_total = {
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    };
-    cur_nutriInfo.forEach((item) => {
-      cur_total.calories += item.nutrients.calories;
-      cur_total.fat += item.nutrients.fat;
-      cur_total.carbs += item.nutrients.carbs;
-      cur_total.protein += item.nutrients.protein;
-    });
-    cur_total.calories = cur_total.calories < 1 ? cur_total.calories : 1;
-    cur_total.fat = cur_total.fat < 1 ? cur_total.fat : 1;
-    cur_total.carbs = cur_total.carbs < 1 ? cur_total.carbs : 1;
-    cur_total.protein = cur_total.protein < 1 ? cur_total.protein : 1;
-    cur_nutriInfo.push({
-      name: "total",
-      nutrients: cur_total,
-    });
-    setNutriInfo(cur_nutriInfo);
-  }, []);
+  useState(() => {
+    setNutriInfo([
+      {
+        name: "Apple",
+        nutrients: {
+          calories: 0.05,
+          fat: 0.0,
+          carbs: 0.2,
+          protein: 0.0,
+        },
+      },
+      {
+        name: "burger",
+        nutrients: {
+          calories: 0.2,
+          fat: 0.6,
+          carbs: 0.3,
+          protein: 0.5,
+        },
+      },
+      {
+        name: "Total",
+        nutrients: {
+          calories: 0.7,
+          fat: 0.6,
+          carbs: 0.5,
+          protein: 0.5,
+        },
+      },
+    ]);
+  });
+
+  // useState(async () => {
+  //   const cur_nutriInfo = await foodHandler.getNutriInfo();
+  //   const cur_total = {
+  //     calories: 0,
+  //     fat: 0,
+  //     carbs: 0,
+  //     protein: 0,
+  //   };
+  //   cur_nutriInfo.forEach((item) => {
+  //     cur_total.calories += item.nutrients.calories;
+  //     cur_total.fat += item.nutrients.fat;
+  //     cur_total.carbs += item.nutrients.carbs;
+  //     cur_total.protein += item.nutrients.protein;
+  //   });
+  //   cur_total.calories = cur_total.calories < 1 ? cur_total.calories : 1;
+  //   cur_total.fat = cur_total.fat < 1 ? cur_total.fat : 1;
+  //   cur_total.carbs = cur_total.carbs < 1 ? cur_total.carbs : 1;
+  //   cur_total.protein = cur_total.protein < 1 ? cur_total.protein : 1;
+  //   cur_nutriInfo.push({
+  //     name: "total",
+  //     nutrients: cur_total,
+  //   });
+  //   setNutriInfo(cur_nutriInfo);
+  // }, []);
 
   const uploadPhoto = async (fd) => {
     setSubmitted(true);
@@ -43,7 +75,7 @@ const App = () => {
   return (
     <div className="h-screen flex-auto bg-base-100 text-black">
       <Navbar></Navbar>
-      <div className="flex justify-center items-center h-full space-x-10">
+      <div className="flex justify-center items-center h-4/5 space-x-10">
         <PhotoForm uploadPhoto={uploadPhoto} submitted={submitted} />
         <Chart nutriInfo={nutriInfo} />
       </div>
--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -2,7 +2,11 @@ import { useState } from "react";
 
 const Chart = ({ nutriInfo }) => {
   const [selectedData, setSelectedData] = useState("total");
-  const dataOptions = nutriInfo.map((item) => item.name);
+  const dataOptions = nutriInfo.map((item) => item.name.toLowerCase());
+  const data = nutriInfo.find(
+    (item) => item.name.toLowerCase() === selectedData
+  );
+  console.log(data);
 
   const today = new Date();
   const date = today.toLocaleDateString("en-US", {
@@ -15,10 +19,11 @@ const Chart = ({ nutriInfo }) => {
     <div className="card w-3/5 shadow-lg bg-white hover:shadow-gray-500">
       <div className="card-body">
         <h2 className="card-title">Nutrient Consumption - {date}</h2>
+
         <select
-          className="form-select w-full max-w-xs"
-          value={selectedData}
-          onChange={(e) => setSelectedData(e.target.value)}
+          className="select select-bordered select-sm w-1/5 bg-white border-gray-500"
+          onChange={(e) => setSelectedData(e.target.value.toLowerCase())}
+          defaultValue={"total"}
         >
           {dataOptions.map((item) => (
             <option key={item} value={item}>
@@ -26,6 +31,39 @@ const Chart = ({ nutriInfo }) => {
             </option>
           ))}
         </select>
+
+        <div className="flex flex-row items-center justify-center space-y-4">
+          <h2 className="w-1/5 ">Calories</h2>
+          <progress
+            className="progress h-12 w-4/5 progress-primary"
+            value={data.nutrients.calories}
+            max="1"
+          ></progress>
+        </div>
+        <div className="flex flex-row items-center justify-center space-y-4">
+          <h2 className="w-1/5 ">Fat</h2>
+          <progress
+            className="progress h-12 w-4/5 progress-primary"
+            value={data.nutrients.fat}
+            max="1"
+          ></progress>
+        </div>
+        <div className="flex flex-row items-center justify-center space-y-4">
+          <h2 className="w-1/5 ">Carbs</h2>
+          <progress
+            className="progress h-12 w-4/5 progress-primary"
+            value={data.nutrients.carbs}
+            max="1"
+          ></progress>
+        </div>
+        <div className="flex flex-row items-center justify-center space-y-4">
+          <h2 className="w-1/5 ">Protein</h2>
+          <progress
+            className="progress h-12 w-4/5 progress-primary"
+            value={data.nutrients.protein}
+            max="1"
+          ></progress>
+        </div>
       </div>
     </div>
   );
